37e2b398d3b9fd377cb044b5fb06cd645a74ed9e
unknown <joe@wephy.com>
2024-08-07 07:59:52 +0100

    basename change

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/spa-demo/"
   plugins: [
     remix({
+      basename: "/spa-demo/",
       ssr: false,
       future: {
         v3_fetcherPersist: true,
