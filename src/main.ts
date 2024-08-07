97006fc404c4f7e931c3f2a7b34c080b503e7812
unknown <joe@wephy.com>
2024-08-07 23:03:07 +0100

    font update

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import './style.css'
 import App from './App.vue'
 
 // Fonts
-import './styles/amulya.css';
-import './styles/synonym.css';
+import './styles/fonts.css';
 
 createApp(App).mount('#app')
